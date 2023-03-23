063cd4b44357200497f448f14f780f4cc8285b35
federicaschena <federicaschena16@gmail.com>
2023-03-23 10:41:01 +0100

    last modification for name changing of the package

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        	'talker = detecion_node.webcam:main'
+        	'talker = detection_node.webcam:main'
         ],
     },
 )
